b1681ffc57fb15e77eaeb4018432e6354a570c89
DaPigGuy <mcpepig123@gmail.com>
2023-06-25 04:33:25 -0700

    Update `Player` data type

--- src/player/data.ts
+++ src/player/data.ts
@@ -1,3 +1,17 @@
+export type Rank =
+	| "REGULAR"
+	| "PLUS"
+	| "YOUTUBER"
+	| "STREAMER"
+	| "TIKTOK"
+	| "VIP"
+	| "HELPER"
+	| "MODERATOR"
+	| "HIVE_TEAM"
+	| "STAFF_MANAGER"
+	| "COMMUNITY_MANAGER"
+	| "OWNER";
+
 export type Avatar = { url: string; name: string };
 
 export type Player = {
@@ -5,28 +19,20 @@ export type Player = {
 	xuid: string;
 	username: string;
 	username_cc: string;
-	rank:
-		| "REGULAR"
-		| "PLUS"
-		| "YOUTUBER"
-		| "STREAMER"
-		| "TIKTOK"
-		| "VIP"
-		| "HELPER"
-		| "MODERATOR"
-		| "HIVE_TEAM"
-		| "STAFF_MANAGER"
-		| "COMMUNITY_MANAGER"
-		| "OWNER";
+	rank: Rank;
 	first_played: number;
 	daily_login_streak: number;
 	longest_daily_login_streak: number;
 	hub_title_count: number;
+	hub_title_unlocked: string[];
 	costume_count: number;
+	costume_unlocked: string[];
 	avatar_count: number;
+	avatar_unlocked: Avatar[];
 	friend_count: number;
 	equipped_hub_title: string | null;
 	equipped_costume: string | null;
 	equipped_avatar: Avatar | null;
+	quest_count: number;
+	paid_ranks: Rank[];
 };
-
